bcc6e0a9c30827c2377ebfddd831a37b09c9d342
Pek Yi Liang <yiliangpek@outlook.com>
2026-03-31 00:40:15 +0800

    improve nowcasting model with cv - partial quarter handling still wip

--- backend/random_forrest_model.py
+++ backend/random_forrest_model.py
@@ -7,58 +7,32 @@ from quarterly_and_monthly_data import get_quarterly_data_rf
 
 # get current file directory
 quarterly_data = get_quarterly_data_rf()
-latest_quarter = pd.DataFrame(quarterly_data.tail(1))
-
 
 df_quarterly = quarterly_data.copy()
-df_quarterly = df_quarterly.dropna()
 df_quarterly.index = pd.to_datetime(df_quarterly.index)
 df_quarterly = df_quarterly.sort_index()
 
-#define x and y for monthly data
-x = df_quarterly.drop(columns=['GDPC1'])
-y = df_quarterly['GDPC1']
-
-
-# spreads and key signals
-x['spread'] = x['BAA'] - x['AAA']
-x['term_spread'] = x['GS10'] - x['TB3MS']
-x['INDPRO_growth'] = np.log(x['INDPRO']).diff()
-x['HOUST_growth'] = np.log(x['HOUST']).diff()
-x['INVEST_growth'] = np.log(df_quarterly['INVEST']).diff()
-x = x[['spread', 'term_spread','INDPRO_growth', 'HOUST_growth', 'UNRATE', 'INVEST_growth']]
-#x = x[['spread', 'term_spread', 'INDPRO_growth', 'HOUST_growth', 'UNRATE']]
-x = x.dropna()
-y = y.loc[x.index]
+# feature engineering on dataset
+x_full = df_quarterly.copy()
+x_full['spread'] = x_full['BAA'] - x_full['AAA']
+x_full['term_spread'] = x_full['GS10'] - x_full['TB3MS']
+x_full['INDPRO_growth'] = np.log(x_full['INDPRO']).diff()
+x_full['HOUST_growth'] = np.log(x_full['HOUST']).diff()
+x_full['INVEST_growth'] = np.log(x_full['INVEST']).diff()
+x_full = x_full[['spread', 'term_spread','INDPRO_growth', 
+                 'HOUST_growth', 'UNRATE', 'INVEST_growth', 'Covid', 'SARS']]
+train_mask = df_quarterly['GDPC1'].notna()
+X = x_full[train_mask]
+y = df_quarterly.loc[train_mask, 'GDPC1']
+X = X.dropna()
+y = y.loc[X.index]
 
 # split into train and test sets (train - first 80% of observations, test - last 20% of observations)
-split_index = int(len(x) * 0.8)
-X_train = x.iloc[:split_index]
-X_test  = x.iloc[split_index:]
+split_index = int(len(X) * 0.8)
+X_train = X.iloc[:split_index]
+X_test  = X.iloc[split_index:]
 y_train = y.iloc[:split_index]
 y_test  = y.iloc[split_index:]
-
-'''
-#Train the random forest model
-rf = RandomForestRegressor(
-    n_estimators=300, #number of trees in the forest
-    max_depth=None, #maximum depth of the tree 
-    min_samples_leaf=1,
-    random_state=42 #for reproducibility (seed)
-)
-rf.fit(X_train, y_train)
-
-#Predict on the test set
-y_pred = rf.predict(X_test)
-rmse = np.sqrt(mean_squared_error(y_test, y_pred))
-print("RMSE:", rmse)
-print("Pred std:", np.std(y_pred))
-print("Actual std:", np.std(y_test))
-results = pd.DataFrame({
-    'Actual': y_test,
-    'Predicted': y_pred
-})
-'''
 
 #find the optimal number of trees (n_estimators) & depth using out-of-bag error
 n_estimators_list = list(range(100, 501, 100))
@@ -83,40 +57,61 @@ for n in n_estimators_list:
             best_rmse = rmse
             best_params = (n, d)
             
-# Train best model
+# Train best model to predict current GDP nowcast
 best_n, best_d = best_params
 rf = RandomForestRegressor(
     n_estimators=best_n,
     max_depth=best_d,
     random_state=42
 )
-rf.fit(X_train, y_train)
+rf.fit(X, y)
+Xcurrent = x_full.tail(1)
+y_pred = rf.predict(Xcurrent)  
 
 # Predict current quarter GDP nowcast
-latest_quarter['spread'] = latest_quarter['BAA'] - latest_quarter['AAA']
-latest_quarter['term_spread'] = latest_quarter['GS10'] - latest_quarter['TB3MS']
-latest_quarter['INDPRO_growth'] = np.log(latest_quarter['INDPRO']).diff()
-latest_quarter['HOUST_growth'] = np.log(latest_quarter['HOUST']).diff()
-latest_quarter['INVEST_growth'] = np.log(latest_quarter['INVEST']).diff()
-latest_quarter = latest_quarter.tail(1)
-Xcurrent = latest_quarter[['spread', 'term_spread','INDPRO_growth', 'HOUST_growth', 'UNRATE', 'INVEST_growth']]
 current_gdp_nowcast = rf.predict(Xcurrent)
 
 print("\nBest RMSE:", best_rmse)
 print("Best Params:", best_params)
 print(f"Current GDP Nowcast ({Xcurrent.index[0].date()}): {current_gdp_nowcast[0]:.4f}")
 
-# Actual vs Predicted GDP - RF model function to be called       
-def predicted_vs_actual_gdp():
-    results = pd.DataFrame({
-        'Actual_GDP': y_test,
-        'Predicted_GDP': y_pred
-    })
-    nowcast_row = pd.DataFrame({
-    'Actual_GDP': [np.nan],
-    'Predicted_GDP': [current_gdp_nowcast[0]]
-    }, index=[Xcurrent.index[0]])
-    results = pd.concat([results, nowcast_row])
-    return results
+# Historical Predictions (CV Approach - RF trained)
+cv_errors = []
+cv_preds = []
+cv_actuals = []
+cv_dates = []
 
-# NOTE: Model is not fully accurate yet, but you can still call this function for BE/FE
+start = int(len(X) * 0.5)
+for i in range(start, len(X)):
+    X_train_cv = X.iloc[:i]
+    y_train_cv = y.iloc[:i]
+    X_test_cv = X.iloc[i:i+1]
+    y_test_cv = y.iloc[i:i+1]
+    rf_cv = RandomForestRegressor(
+        n_estimators=best_n,
+        max_depth=best_d,
+        random_state=42
+    )
+    rf_cv.fit(X_train_cv, y_train_cv)
+    y_pred_cv = rf_cv.predict(X_test_cv)
+    error = (y_test_cv.values[0] - y_pred_cv[0])**2
+    cv_errors.append(error)
+    cv_preds.append(y_pred_cv[0])
+    cv_actuals.append(y_test_cv.values[0])
+    cv_dates.append(X_test_cv.index[0])
+
+cv_rmse = np.sqrt(np.mean(cv_errors))
+print("CV RMSE:", cv_rmse)
+
+nowcast_row = pd.DataFrame({
+    "Date": [Xcurrent.index[0]],
+    "Actual": [np.nan],
+    "Predicted": [current_gdp_nowcast[0]]
+}).set_index("Date")
+cv_results = pd.DataFrame({
+    "Date": cv_dates,
+    "Actual": cv_actuals,
+    "Predicted": cv_preds
+}).set_index("Date")
+cv_results = pd.concat([cv_results, nowcast_row])
+print(cv_results.tail(30))
